a5cd06397d38c17d51084161c459032af027532a
yoni-r <mwolson@gnu.org>
2006-02-16 18:18:00 +0000

    update README file
    darcs-hash:20060216181847-6ce53-04e7a1078c0eb8e134ad7b20d0d37ed751407c4c.gz

--- emms-stream-info.el
+++ emms-stream-info.el
@@ -1,11 +1,11 @@
 ;;; emms-stream-info.el --- Show what is currently playing on a
 ;;; streaming audio station.
 
-;; Copyright (C) 2004, 2005 Yoni Rabkin Katzenell <yoni-r@actcom.com>
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2 of the
-;; License, or (at your option) any later version.
+;; Copyright (C) 2004, 2005, 2006 Yoni Rabkin Katzenell
+;; <yoni-r@actcom.com> This program is free software; you can
+;; redistribute it and/or modify it under the terms of the GNU General
+;; Public License as published by the Free Software Foundation; either
+;; version 2 of the License, or (at your option) any later version.
 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
