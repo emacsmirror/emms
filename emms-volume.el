ad3d6bb6d898cc42f673b99ea3721357d62c7d8f
Michael Olson <mwolson@gnu.org>
2006-06-08 19:41:00 +0000

    Standardize copyright notices.  Add COPYING file.  Mention license in README.
    darcs-hash:20060608194123-1bfb2-cbfbc9eacb41016b24acb9ea2654f141496b0caa.gz

--- emms-volume.el
+++ emms-volume.el
@@ -4,23 +4,25 @@
 
 ;; Author: Martin Schoenmakers <aiviru@diamond-age.net>
 
-;; This file is free software; you can redistribute it and/or modify
+;; This file is part of EMMS.
+
+;; EMMS is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
 ;; any later version.
 ;;
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; EMMS is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING. If not, write to the
+;; along with EMMS; see the file COPYING. If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
-;; Boston, MA 02110-1301 USA
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
-;; 
+;;
 ;; This file provides generally two things:
 ;; Generic volume setting functions and some appropriate bindings for EMMS
 ;; playlist buffers. These can also be bound to global keys,however, the
