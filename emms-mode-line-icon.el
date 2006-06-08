817f4fad783bbf5cde049a65e873bd824c451a35
Michael Olson <mwolson@gnu.org>
2006-06-08 19:41:00 +0000

    Standardize copyright notices.  Add COPYING file.  Mention license in README.
    darcs-hash:20060608194123-1bfb2-cbfbc9eacb41016b24acb9ea2654f141496b0caa.gz

--- emms-mode-line-icon.el
+++ emms-mode-line-icon.el
@@ -8,20 +8,21 @@
 ;; Author: Daniel Brockman <daniel@brockman.se>
 ;; Maintainer: Lucas Bonnet <lucas@rincevent.net>
 
-;; This program is free software; you can redistribute it and/or
+;; This file is part of EMMS.
+
+;; EMMS is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
 ;; as published by the Free Software Foundation; either version 2
 ;; of the License, or (at your option) any later version.
 
-;; This program is distributed in the hope that it will be useful,
+;; EMMS is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
-;; 02110-1301 USA
+;; along with EMMS; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 
 ;; Commentary: 
 
