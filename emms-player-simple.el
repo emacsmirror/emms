0e94a4481af4582b90e7e3d775acb3a2069cd37f
mlang <mwolson@gnu.org>
2006-09-29 23:14:00 +0000

    midi-files-via-timidity
    A simple-player definition for timidity
    
    darcs-hash:20060929231452-08870-791180c4ae2db7c713362b97bc4053c22e3c4888.gz

--- emms-player-simple.el
+++ emms-player-simple.el
@@ -178,6 +178,9 @@ PLAYER is the name of the current player."
                              ".imf" ".mod" ".med" ".mtm" ".okt" ".s3m"
                              ".stm" ".stx" ".ult" ".apun" ".xm" ".mod")
   "mikmod" "-q" "-p" "1" "-X")
+(define-emms-simple-player timidity '(file)
+  (emms-player-simple-regexp "mid" "rmi" "rcp" "r36" "g18" "g36" "mfi")
+  "timidity")
 (define-emms-simple-player alsaplayer '(file url)
   (emms-player-simple-regexp ".ogg" ".mp3" ".wav" ".flac" ".pls" ".m3u" "http://")
   "alsaplayer" "--quiet" "--nosave" "\"--interface text\"")
