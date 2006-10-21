604cc12d9808440cd68454239a204ababfb01055
william.xwl <mwolson@gnu.org>
2006-10-21 06:23:00 +0000

    emms-player-mplayer.el: Mplayer also supports .vob files.
    darcs-hash:20061021062319-e8fe6-cc708b808381325149b0845d6778778afd33a65f.gz

--- emms-player-mplayer.el
+++ emms-player-mplayer.el
@@ -33,7 +33,7 @@
 (define-emms-simple-player mplayer '(file url)
   (regexp-opt '(".ogg" ".mp3" ".wav" ".mpg" ".mpeg" ".wmv" ".wma"
                 ".mov" ".avi" ".divx" ".ogm" ".asf" ".mkv" "http://" "mms://"
-		".rm" ".rmvb" ".mp4" ".flac"))
+		".rm" ".rmvb" ".mp4" ".flac" ".vob"))
   "mplayer" "-slave" "-quiet" "-really-quiet")
 
 (define-emms-simple-player mplayer-playlist '(streamlist)
