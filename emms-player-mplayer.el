6b2d7d265401b6e2844c5b27c565fea9f942d7c9
Damien Elmes <emms@repose.cx>
2007-05-11 21:35:00 +0000

    add .m4a support to mplayer plugin
    darcs-hash:20070511213548-4e3e3-e42d9cb5d377280372f6c4b183a4f0d4d85da2ac.gz

--- emms-player-mplayer.el
+++ emms-player-mplayer.el
@@ -33,7 +33,7 @@
 (define-emms-simple-player mplayer '(file url)
   (regexp-opt '(".ogg" ".mp3" ".wav" ".mpg" ".mpeg" ".wmv" ".wma"
                 ".mov" ".avi" ".divx" ".ogm" ".asf" ".mkv" "http://" "mms://"
-		".rm" ".rmvb" ".mp4" ".flac" ".vob"))
+                ".rm" ".rmvb" ".mp4" ".flac" ".vob" ".m4a"))
   "mplayer" "-slave" "-quiet" "-really-quiet")
 
 (define-emms-simple-player mplayer-playlist '(streamlist)
