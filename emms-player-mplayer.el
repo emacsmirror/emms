8146cac6e0ad1e44ea24bba7509c9d714fccfbd3
William Xu <william.xwl@gmail.com>
2007-11-21 11:28:00 +0000

    MPlayer supports .ape files now.(at least from 1.0rc2)
    darcs-hash:20071121112846-cfa61-455883f0a3185b29f4bd381ed77ee50575538045.gz

--- emms-player-mplayer.el
+++ emms-player-mplayer.el
@@ -40,7 +40,7 @@
 (define-emms-simple-player mplayer '(file url)
   (regexp-opt '(".ogg" ".mp3" ".wav" ".mpg" ".mpeg" ".wmv" ".wma"
                 ".mov" ".avi" ".divx" ".ogm" ".asf" ".mkv" "http://" "mms://"
-                ".rm" ".rmvb" ".mp4" ".flac" ".vob" ".m4a"))
+                ".rm" ".rmvb" ".mp4" ".flac" ".vob" ".m4a" ".ape"))
   "mplayer" "-slave" "-quiet" "-really-quiet")
 
 (define-emms-simple-player mplayer-playlist '(streamlist)
