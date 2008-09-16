fec94b813d20d571a7cf69a9c7913c889160583d
Tom Rauchenwald <its.sec@gmx.net>
2008-09-15 20:57:29 -0700

    emms-info-libtag: Ignore taglib warnings.

--- lisp/emms-info-libtag.el
+++ lisp/emms-info-libtag.el
@@ -58,7 +58,7 @@
       (when (zerop
              (let ((coding-system-for-read 'utf-8))
                (call-process "emms-print-metadata" 
-                             nil t nil
+                             nil '(t nil) nil
                              (emms-track-name track))))
         (goto-char (point-min))
         ;; Crush the trailing whitespace
