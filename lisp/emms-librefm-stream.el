431595d1f1d8e789a3fdedc95223c9d45afbf114
Yoni Rabkin <yonirabkin@member.fsf.org>
2014-04-01 14:49:18 -0400

    * lisp/emms-librefm-stream.el: Add file.

---
+++ lisp/emms-librefm-stream.el
@@ -0,0 +1,39 @@
+;;; emms-librefm-stream.el --- Libre.FM streaming
+
+;; Copyright (C) 2014  Free Software Foundation, Inc.
+
+;; Author: Yoni Rabkin <yrk@gnu.org>
+
+;; Keywords: emms, libre.fm, GNU FM
+
+;; EMMS is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; EMMS is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with EMMS; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+;; MA 02110-1301, USA.
+
+
+;;; Code:
+
+(require 'emms-librefm-scrobbler)
+
+(defvar emms-librefm-stream-host-url
+  "alpha.libre.fm"
+  "URL for the streaming host")
+
+
+
+
+(provide 'emms-librefm-stream)
+
+
+;;; emms-librefm-stream.el ends here
