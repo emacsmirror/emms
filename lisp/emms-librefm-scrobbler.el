emacsmirror/emms

commit 428d6628917a3a413a0da30be2192c8822cc5db1

emms-librefm-scrobbler.el: Add Commentary-section
Add minimal instructions on how to enable scrobbling.

lisp/emms-librefm-scrobbler.el
@@ -21,6 +21,11 @@
 ;; Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 ;; MA 02110-1301, USA.
 
+;;; Commentary:
+
+;; To use libre.fm you need to add username and password to
+;; ~/.authinfo.gpg or an equivalent file understood by auth-source.
+;; To enable scrobbling call (emms-librefm-scrobbler-enable).
 
 ;;; Code:
 

commit a2f4ee1aeaf433801b57163829b969b5a055949a

Fix bug in introduced in 428d662
- emms.texinfo: used wrong variable.
- emms-librefm-scrobbler.el: remove warning.

doc/emms.texinfo
@@ -1280,7 +1280,7 @@ file, typically @file{~/.authinfo.gpg}, as:
 machine libre.fm login USERNAME password PASSWORD
 @end smallexample
 If you are using some other server than libre.fm, change
-``@example{libre.fm}'' to match
+``@samp{libre.fm}'' to match
 @var{emms-librefm-scrobbler-handshake-url}.
 
 Alternatively, you can save the password in plaintext in your init-file

lisp/emms-librefm-scrobbler.el
@@ -30,6 +30,7 @@
 ;;; Code:
 
 (require 'emms-playing-time)
+(require 'auth-source)
 
 
 (defvar emms-librefm-scrobbler-handshake-url
@@ -86,7 +87,6 @@ See also `emms-librefm-scrobbler-username'.")
 TOKEN is :user of :secret."
   ;; TODO: Maybe we should enable :create t here.  But it could be
   ;; kind of annoying as it makes a pop-up when no name is present.
-  (require 'auth-source)
   (plist-get
    (car (auth-source-search :host (list emms-librefm-scrobbler-handshake-url "libre.fm")
                             :user (unless (equal emms-librefm-scrobbler-username "")