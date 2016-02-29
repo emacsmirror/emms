8ba0a5eb1991544ef91ea90772b614f9ab7d953c
Yoni Rabkin <yrk@gnu.org>
2016-02-28 22:03:16 -0500

    * lisp/emms-show-all.el: new file

---
+++ lisp/emms-show-all.el
@@ -0,0 +1,36 @@
+;;; emms-show-all.el --- Detailed track information for Emms.
+
+;; Copyright (C) 2016  Free Software Foundation, Inc.
+
+;; Author: Yoni Rabkin <yrk@gnu.org>
+
+;; This file is part of EMMS.
+
+;; EMMS is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; EMMS is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with EMMS; if not, write to the Free Software Foundation,
+;; Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;;
+;;; 
+
+;;; Code:
+
+(require 'emms-tag-editor)
+
+
+
+
+(provide 'emms-show-all)
+
+;;; emms-playlist-mode.el ends here
