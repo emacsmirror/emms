a6aa6ca703336ab3fd26f25f36c45894d8ecea5f
Yoni Rabkin <yoni@rabkins.net>
2017-06-19 14:56:40 -0400

    * lisp/emms-volume.el: fix typo
    trivial patch by David Michael <fedora.dm0@gmail.com>

--- lisp/emms-volume.el
+++ lisp/emms-volume.el
@@ -61,7 +61,7 @@
 If you have your own functions for changing volume, set this."
   :type '(choice (const :tag "Amixer" emms-volume-amixer-change)
                  (const :tag "MPD" emms-volume-mpd-change)
-                 (const :tag "Pulseaudio" emms-volume-pule-change)
+		 (const :tag "PulseAudio" emms-volume-pulse-change)
                  (function :tag "Lisp function"))
   :group 'emms-volume)
 
