fb00718c526f96405da020721ceec2d4fcae1190
Yoni Rabkin <yoni@rabkins.net>
2020-05-29 13:38:08 -0400

    * emms.el: interface to elpa
    Yes, it unfortunately carries the same name as lisp/emms.el

---
+++ emms.el
@@ -0,0 +1,35 @@
+;;; emms.el --- The Emacs Multimedia System
+
+;; Copyright (C) 2020 Free Software Foundation, Inc.
+
+;; Author: the Emms developers (see AUTHORS file)
+;; Maintainer: Yoni Rabkin <yrk@gnu.org>
+;; Version: 5.4
+;; Keywords: emms, mp3, ogg, flac, music, mpeg, video, multimedia
+;; Package-Requires: ((cl-lib "0.5"))
+;; url: https://www.gnu.org/software/emms/
+
+;; This file is part of EMMS.
+
+;; EMMS is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; EMMS is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with EMMS; see the file COPYING.  If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+
+;; This file serves as an interface to ELPA, so that Emms can be
+;; distributed as an Emacs Lisp Package. The following line ensures
+;; that the code in the "lisp" directory is loaded.
+;;;###autoload (load "lisp/emms-auto" nil 'nowarn)
+
+
+;;; emms.el ends here.
