6a1c281b009b763230e5e79707b58eb32624156d
Yoni Rabkin <yoni@rabkins.net>
2021-03-03 11:17:03 -0500

    * emms-player-mplayer.el: lexical declaration

--- emms-player-mplayer.el
+++ emms-player-mplayer.el
@@ -1,4 +1,4 @@
-;;; emms-player-mplayer.el --- mplayer support for EMMS
+;;; emms-player-mplayer.el --- mplayer support for EMMS  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2005, 2006, 2007, 2008, 2009 Free Software Foundation, Inc.
 
