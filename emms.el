a435df9461bd141848504418b4bc7bcea7cc881c
Yoni Rabkin <yoni@rabkins.net>
2021-05-12 10:07:13 -0400

    * emms.el: version bump

--- emms.el
+++ emms.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jorgen Schäfer <forcer@forcix.cx>, the Emms developers (see AUTHORS file)
 ;; Maintainer: Yoni Rabkin <yrk@gnu.org>
-;; Version: 7.1
+;; Version: 7.2
 ;; Keywords: emms, mp3, ogg, flac, music, mpeg, video, multimedia
 ;; Package-Type: multi
 ;; Package-Requires: ((cl-lib "0.5") (nadvice "0.3") (seq))
