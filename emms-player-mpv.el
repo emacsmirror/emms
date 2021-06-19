emacsmirror/emms

commit 02915f7aa24d27d717ff4821a58f791cffcc174c

* emms-player-mpv.el: use closure
patch by Stefan Monnier

emms-player-mpv.el
@@ -814,8 +814,8 @@ if there was any issue when trying to start it initially."
       (let*
           ((start-cmd (list (if track-is-playlist 'loadlist 'loadfile)
                             track-name 'replace))
-           (start-func `(lambda ()
-                          (emms-player-mpv-cmd ',start-cmd
+           (start-func (lambda ()
+                          (emms-player-mpv-cmd start-cmd
                                                (apply-partially #'emms-player-mpv-start-error-handler ',start-cmd)))))
         (if emms-player-mpv-ipc-stop-command
             (setq emms-player-mpv-ipc-stop-command start-func)

commit 1ce322a085b0852999b8569b1656a557c6c8bb33

* emms-player-mpv.el: fix reconnect-on-playback-start bug introduced in 02915f7

emms-player-mpv.el
@@ -825,7 +825,7 @@ if there was any issue when trying to start it initially."
                             track-name 'replace))
            (start-func (lambda ()
                           (emms-player-mpv-cmd start-cmd
-                                               (apply-partially #'emms-player-mpv-start-error-handler ',start-cmd))
+                                               (apply-partially #'emms-player-mpv-start-error-handler start-cmd))
                           (emms-player-mpv-cmd '(set pause no)))))
         (if emms-player-mpv-ipc-stop-command
             (setq emms-player-mpv-ipc-stop-command start-func)