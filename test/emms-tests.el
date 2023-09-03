75f1ee292edc141ef4342a2d6640d433e521ff41
Petteri Hintsanen <petterih@iki.fi>
2023-09-03 23:21:57 +0300

    Add copyright information

--- test/emms-tests.el
+++ test/emms-tests.el
@@ -1,4 +1,27 @@
-;; -*- lexical-binding: t; -*-
+;;; emms-tests.el --- Test suite for EMMS core  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Free Software Foundation, Inc.
+
+;; Author: Petteri Hintsanen <petterih@iki.fi>
+
+;; This file is part of EMMS.
+
+;; EMMS is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; EMMS is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with EMMS; see the file COPYING. If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+;; MA 02110-1301, USA.
+
+;;; Code:
 
 (require 'emms)
 (require 'ert)
