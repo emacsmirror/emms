c0f035e98e5bb094b21816689e64185b3311001b
Yoni Rabkin <yrk@gnu.org>
2025-01-20 08:17:11 -0500

    emms-info-exiftool.el: ensure numeric album titles are strings.
    Patch by Richard Sent

--- emms-info-exiftool.el
+++ emms-info-exiftool.el
@@ -90,7 +90,8 @@
 			emms-field
 			(cond ((eq emms-field 'info-playing-time)
 			       (emms-info-exiftool-time))
-			      ((memq emms-field '(info-tracknumber
+			      ((memq emms-field '(info-album
+                                                  info-tracknumber
 						  info-title
 						  info-year
 						  info-discnumber))
