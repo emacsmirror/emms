9f0286cb9e6e8cac52109db1a5a7d5c4de338436
Erica Lina <ericalina@proton.me>
2025-05-30 22:23:24 +0200

    change cl to cl-lib

--- emms-filters.el
+++ emms-filters.el
@@ -480,7 +480,7 @@
 ;; (emms-filters-make-filter-ring '("Tango" "Vals" "Milonga"))
 
 ;;; Code:
-(require 'cl)  ; for lexical-let
+(require 'cl-lib)  ; for lexical-let
 (require 'emms-browser)
 
 (defvar  emms-filters-stack nil
@@ -1327,19 +1327,20 @@ Filter should be a filter cons in the form of '(name . function)."
   (setq emms-filters-current-filter-name (car filter))
   (setq emms-filters-current-filter filter))
 
-(defun emms-filters-browse-by ()
-  "The single interface to emms-browser. Re-render please.
-Uses the top level type, or the default browse type."
-  (emms-browse-by (or emms-browser-top-level-type
-                      emms-browser-default-browse-type)))
+;; (defun emms-filters-browse-by ()
+;;   "The single interface to emms-browser. Re-render please.
+;; Uses the top level type, or the default browse type."
+;;   (emms-browse-by (or emms-browser-top-level-type
+;;                       emms-browser-default-browse-type)))
 
 (defun  emms-filters-refilter ()
   "Make a multi-filter function from the current meta-filter and set it.
 Run the filter changed hook. Ask the browser to re-render."
   (emms-filters-set-filter (cons (caar emms-filters-stack)
-                        (emms-filters-make-multi-filter (cdar emms-filters-stack))))
+                     (emms-filters-make-multi-filter (cdar emms-filters-stack))))
   (run-hooks 'emms-filters-filter-changed-hook)
-  (emms-filters-browse-by)
+  ;; emms-filters-render-hash
+  (emms-browser-browse-by)
   ;; If it is a search or a filter expand the results.
   (when (or emms-filters-stack emms-filters-search-caches)
     (emms-browser-expand-all)))
