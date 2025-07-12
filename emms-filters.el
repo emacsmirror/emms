f0a65a9245ded99ff03402013ad2d31a28af2d89
Yoni Rabkin <yrk@gnu.org>
2025-07-12 17:32:03 -0400

    Update copyright notices and license headers.

--- emms-filters.el
+++ emms-filters.el
@@ -1,7 +1,24 @@
 ;;; emms-filters.el --- Filters for Emms          -*- lexical-binding: t; -*-
-;; Copyright (C) 2023 2024 2025
+
+;; Copyright (C) 2023-2025  Free Software Foundation, Inc.
+;;
 ;; Author:  Erica Lina Qi <EricaLinaQi@proton.me>
-;; Keywords: emms, filter, search, cache, stack
+
+;; This file is part of EMMS.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;; Commentary:
 ;; This code allows you to filter and search the metadata cache.
